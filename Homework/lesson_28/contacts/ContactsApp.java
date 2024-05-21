c1823ad933828685b77e58305226ee8db974351e
zygomaticontheway <mrzygomatic@gmail.com>
2024-05-21 22:36:56 +0200

    Lesson 28 HW task1 words counter

--- Homework/lesson_28/contacts/ContactsApp.java
+++ Homework/lesson_28/contacts/ContactsApp.java
@@ -4,7 +4,7 @@ public class ContactsApp {
     public static void main(String[] args) {
         Contacts contacts = new Contacts();
         contacts.addContact(new Contact("Neo", "+4915110009900"));
-        contacts.addContact(new Contact("Morphius", "+4915110009901"));
+        contacts.addContact(new Contact("Morpheus", "+4915110009901"));
         contacts.addContact(new Contact("Trinity", "+4915110009902"));
     }
 
