a72bba9c2ef76b1297ca9fdd3fc2460b658c87b1
zygomaticontheway <mrzygomatic@gmail.com>
2024-05-29 16:36:34 +0200

    Lesson 34 group task Order fix

--- lesson_34/code/orders/OrdersCollection.java
+++ lesson_34/code/orders/OrdersCollection.java
@@ -24,7 +24,7 @@ public class OrdersCollection {
     }
     public Order searchByOrderID (Integer orderId){
         for (Order order : orders){
-            if (order.getOrderID() == orderId){
+            if (order.getOrderID().equals(orderId)){
                 return order;
             }
         }
